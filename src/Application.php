f4723f798cb5b1cdb419a7968893c2b2f48741b3
lozovoyv <lozovoyv@gmail.com>
2021-02-04 15:38:46 +0300

    add profiling

--- src/Application.php
+++ src/Application.php
@@ -16,6 +16,12 @@ class Application
     /** @var ContainerInterface Bound container */
     protected ContainerInterface $container;
 
+    /** @var array Profiling application */
+    protected array $profiling = [];
+
+    /** @var float Timestamp of application start */
+    protected float $startTime;
+
     /**
      * Application constructor.
      *
@@ -24,10 +30,37 @@ class Application
      */
     public function __construct(ContainerInterface $container, string $basePath)
     {
+        $this->startTime = hrtime(true);
+        $this->profiling('app.constructor start');
+
         $this->setBasePath($basePath);
 
         $this->container = $container;
         $this->container->instance('app', $this);
+
+        $this->profiling('app.constructor finish');
+    }
+
+    /**
+     * Write action to profiling or get whole profiling list.
+     *
+     * @param string|null $action
+     *
+     * @return  array|null
+     */
+    public function profiling(?string $action): ?array
+    {
+        if ($action === null) {
+            return $this->profiling;
+        }
+
+        $this->profiling[] = [
+            'action' => $action,
+            'time' => hrtime(true) - $this->startTime,
+            'memory' => memory_get_usage(true)
+        ];
+
+        return null;
     }
 
     /**
@@ -40,6 +73,7 @@ class Application
     protected function setBasePath(string $basePath): void
     {
         $this->basePath = rtrim($basePath, '\/');
+        $this->profiling('app.setBasePath');
     }
 
     /**
@@ -64,6 +98,8 @@ class Application
      */
     public function init(): void
     {
+        $this->profiling('app.init start');
+
         // Resolve, initialize and load config.
         // Config repository, cache and environment drivers are resolved from container and must be bound outside
         // application in bootstrap file.
@@ -72,6 +108,8 @@ class Application
         $config = $this->container->make(ConfigInterface::class);
         $config->load();
         $this->container->instance('config', $config);
+
+        $this->profiling('app.init finish');
     }
 
     /**
@@ -81,6 +119,8 @@ class Application
      */
     public function config(): ConfigInterface
     {
+        $this->profiling('app.config get');
+
         return $this->container->make('config');
     }
 }
